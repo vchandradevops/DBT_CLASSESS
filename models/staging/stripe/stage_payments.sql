2ef3ad513284933997193adc8c1b0cf4ba80aa05
chandra202102@gmail.com <chandra202102@gmail.com>
2025-11-14 13:45:49 +0000

    3time changes

---
+++ models/staging/stripe/stage_payments.sql
@@ -0,0 +1,7 @@
+select id as payment_id,
+orderid as order_id,
+paymentmethod as payment_method,
+status,
+--amount is stored in cents converting into dollors
+amount/100 as amount,
+created as created_at from raw.stripe.payment
